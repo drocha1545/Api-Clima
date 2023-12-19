4e54fbc0574b126124c941c955167bcdff343243
David Emiliano Rocha Flores <david_rocha@itstepuniv.onmicrosoft.com>
2023-12-18 17:08:08 -0800

    Actualizado HTML, CSS y archivos del servidor

--- src/server.js
+++ src/server.js
@@ -1,24 +1,19 @@
+
 const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
 const app = express();
 const port = 3000;
 
-// Usa CORS para permitir el acceso a tu API desde tu front-end
 app.use(cors());
-
-// Middleware para servir archivos estáticos desde la carpeta 'public'
 app.use(express.static('public'));
 
-// Ruta para manejar las solicitudes a '/weather'
 app.get('/weather', (req, res) => {
-    const parameters = 't_2m:C,precip_1h:mm,wind_speed_10m:ms'; // Parámetros meteorológicos que quieres
-    const location = req.query.location; // Ubicación proporcionada por el usuario
-    
-    // Fecha y hora actuales en formato ISO para obtener el clima en vivo
-    const date = new Date().toISOString(); 
+    const location = req.query.location;
+    const parameters = 't_2m:C,precip_1h:mm,wind_speed_10m:ms';
+    const date = new Date().toISOString();
     const username = 'educavi_rocha_david'; // Tu nombre de usuario de Meteomatics
-    const password = 'd3I35lRi9F'; // Tu contraseña de Meteomatics
+    const password = 'd3I35lRi9F'; 
 
     const url = `https://api.meteomatics.com/${date}/${parameters}/${location}/json`;
 
@@ -32,11 +27,11 @@ app.get('/weather', (req, res) => {
         res.json(response.data);
     })
     .catch(error => {
-        console.error('Error al realizar la solicitud a la API:', error);
-        res.status(500).json({ error: error.message });
+        console.error('Error:', error);
+        res.status(500).send('Error al consultar la API de clima');
     });
 });
 
 app.listen(port, () => {
-    console.log(`Servidor escuchando en http://localhost:${port}`);
+    console.log(`Servidor ejecutándose en http://localhost:${port}`);
 });
